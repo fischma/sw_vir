076754da9bfaac5d0808721cc394d840a68173c1
Bara <maskobar@fel.cvut.cz>
2021-01-04 14:52:00 +0100

    okomentovano a vycisteno od zbytecnych printu

--- blackbox.py
+++ blackbox.py
@@ -1,11 +1,13 @@
 import numpy as np
 import os
 import random
-from f10 import f10RaceCarEnv
+from Semestralka.f10 import f10RaceCarEnv
 
-
-# This script will run correctly assuming you have pythonpath pointing to the src directory. If you open a pycharm project inside the hw_1 file from sources then pythonpath should be set automatically
-# Otherwise you can export PYTHONPATH to add the src directory in the terminal as follows: export PYTHONPATH=${PYTHONPATH}:/home/.../hw_1
+'''
+Training the car
+sources: VIR hw1
+modified for our purposes
+'''
 
 class PolicyNet():
     def __init__(self, obs_dim, act_dim):
@@ -13,25 +15,21 @@ class PolicyNet():
         self.b = np.zeros(act_dim)
         self.obsDim = obs_dim
         self.actDim = act_dim
-        # Initialize your weight parameters of your policy.
-        # The simplest policy to implement would be a simple affine mapping y = xw+b, where x is the input,
-        # w is the weight matrix and b is the bias
 
     def set_params(self, w):
         # This function takes in a list w and maps it to your policy parameters.
-        # The simplest way is to probably make an array out of the w vector and reshape it appropriately
         dim = self.obsDim*self.actDim
         self.b = w[dim:]
         self.w = np.reshape(w[:dim], (self.actDim, self.obsDim))
 
     def get_params(self):
-        # This function returns a list w from your policy parameters. You can use numpy's flatten() function
+        # This function returns a list w from your policy parameters.
         w = np.concatenate((self.w.flatten(), self.b.T))
         return w
 
     def forward(self, x):
         a = self.w @ x + self.b
-        # Performs the forward pass on your policy. Maps observation input x to action output a
+        # Performs the forward pass on your policy.
         return a
 
 
@@ -42,24 +40,30 @@ def f_wrapper(env, policy):
         obs = env.reset()
         # Map the weight vector to your policy
         policy.set_params(w)
+        maxvel = 0
+        avgvel = 0
+        pocet = 0
         while not done:
             # Get action from policy
             act,vel = policy.forward(obs)
             # Step environment
-            #print("bef",vel)
             vel_cliped = np.clip(vel, 0, 1)
-            vel = vel_cliped*60
-            #print(vel)
+            vel = vel_cliped*100
+            avgvel += vel
+            pocet +=1
+            if vel > maxvel:
+                maxvel = vel
             obs, rew, done = env.step(act, vel_cliped)
             reward += rew
+        print("Max=",maxvel)
+        avg = avgvel/pocet
+        print("AVG = ",avg)
         return reward
 
     return f
 
 
 def my_opt(f, w_init, iters):
-    # Your optimization algorithm. Takes in an evaluation function f, and initial solution guess w_init and returns
-    # parameters w_best which 'solve' the problem to some degree.
     w_best = w_init
     r_best = 0
     for i in range(iters):
@@ -95,16 +99,15 @@ def test(w_best, max_steps=70, animate=False):
 if __name__ == "__main__":
     train = True
     #train = False
-    policy_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'f10_bbx.npy')
+    policy_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'f10_bbx300_2.npy') #using pregenerated tracks
     max_steps = 1000
-    N_training_iters =1000
+    N_training_iters = 100
     w_best = None
     if train:
-        # Make the environment and your policy
+
         env = f10RaceCarEnv(animate=False, max_steps=max_steps)
         policy = PolicyNet(env.obs_dim, env.act_dim)
 
-        # Make evaluation function
         f = f_wrapper(env, policy)
 
         # Initial guess of solution
